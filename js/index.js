17c5f1aed5b5abebd5857be5ef9e7884723929c3
Mike Mytkowski <maddog1138@gmail.com>
2024-01-11 15:15:15 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function getRandomLetter() {
+    return (Letters[Math.floor(Math.random()*26)])
+}
+
+
+function scrambleText(event){
+
+    let iterations = 0;
+    let codenameLength = event.target.dataset.value.length;
+
+    const interval = setInterval(() =>{
+    let codename = event.target.innerText.split("").map((letter,index) =>{
+        if(index <= iterations ) {
+            return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+
+    } );
+    event.target.innerText = codename.join("");
+    console.log ("updated codename is: " + codenameLength);
+
+    if(iterations > codenameLength) clearInterval(interval);
+    iterations += 1/5;
+
+}, 30 );
+
+}
+
+
+document.querySelector("#codename").onmouseover = scrambleText;
